6c22150fd3d786de8488fccbb4ccb2facef6be2a
Freek Kalter <freek@kalteronline.org>
2012-10-18 18:38:01 +0200

    [split-image] default destination folder is ~/Pictures/wallpapers

--- scripts/split-image.pl
+++ scripts/split-image.pl
@@ -4,4 +4,4 @@ use File::Basename;
 
 my $img = shift;
 my ( $base, $form ) = basename($img) =~ /(.+)\.(.+)/;
-say `convert "$img" -crop %50x0% +repage "$base-\%d.$form"`; 
+say `convert "$img" -crop %50x0% +repage "/home/fkalter/Pictures/wallpapers/$base-\%d.$form"`; 
