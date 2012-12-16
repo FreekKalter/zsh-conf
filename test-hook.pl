a149402b61e67cb676cdfdf107210ed5a6911d1c
Freek Kalter <freek@kalteronline.org>
2012-12-16 12:03:54 +0100

    system independent test-hook

--- test-hook.pl
+++ test-hook.pl
@@ -3,17 +3,7 @@ use v5.14;
 use File::Spec;
 
 my %tmpENV = (
-          'GIT_AUTHOR_NAME' => 'Freek Kalter',
-          'PWD' => '/home/fkalter/github/zsh-conf',
-          'GIT_PREFIX' => '',
-          'PATH' => '/usr/lib/git-core:/home/fkalter/perl5/perlbrew/bin:/home/fkalter/perl5/perlbrew/perls/perl-5.16.1/bin:/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/fkalter/scripts:/home/fkalter/scripts',
           'GIT_DIR' => "$ENV{PWD}/.git" ,
-          'GIT_AUTHOR_EMAIL' => 'freek@kalteronline.org',
-          'GIT_INDEX_FILE' => '.git/index',
-          'GIT_EDITOR' => ':',
-          'OLDPWD' => '/home/fkalter/github/zsh-conf/.git/hooks',
-          '_' => '/usr/bin/git',
-          'GIT_AUTHOR_DATE' => '@1351534578 +0100'
 );
 local %ENV = (%ENV, %tmpENV);
 my $exec = $ARGV[0] || "$ENV{PWD}/.git/hooks/post-commit";
