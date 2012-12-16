5f610385f239c917ce1d9456d078dff98fcce5a4
Freek Kalter <freek@kalteronline.org>
2012-12-16 14:30:30 +0100

    call hook scripts whitout git, no more test-hook.pl

--- hook_create_script.pl
+++ hook_create_script.pl
@@ -5,6 +5,12 @@ use Cwd;
 use File::Spec;
 
 my $script= qq{#!/bin/sh
+if [ -z \$GIT_DIR ]; then
+    SCRIPT_LOC=\$(readlink -f \$0)
+    SCRIPT_PATH=`dirname \$SCRIPT_LOC`
+    GIT_DIR="\$SCRIPT_PATH/.."
+fi
+
 ln -f \$GIT_DIR/../.zshrc ~/.zshrc
 ln -f \$GIT_DIR/../.zsh_aliases ~/.zsh_aliases
 ln -f \$GIT_DIR/../.tmux.conf ~/.tmux.conf
