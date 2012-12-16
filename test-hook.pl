3bbdafbe2bd1fd2aa7350dc083f78ae65014c548
Freek Kalter <freek@kalteronline.org>
2012-12-16 13:55:49 +0100

    use cwd for test-hook

--- test-hook.pl
+++ test-hook.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/env perl
 use v5.14;
 use File::Spec;
+use Cwd;
 
 my %tmpENV = (
-          'GIT_DIR' => "$ENV{PWD}/.git" ,
+          'GIT_DIR' =>  getcwd() . "/.git" ,
 );
 local %ENV = (%ENV, %tmpENV);
-my $exec = $ARGV[0] || "$ENV{PWD}/.git/hooks/post-commit";
-say $exec;
+my $exec = $ARGV[0] || getcwd() . "/.git/hooks/post-commit";
 `$exec`;
