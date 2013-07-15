cb6b1473e4b59894c6cd02b623113880b402fc1e
Freek Kalter <freek@kalteronline.org>
2013-07-15 19:18:21 +0200

    build go every 5 days from tip

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,3 +2,13 @@
 sudo truecrypt --auto-mount=favorites --keyfiles=/mnt/secret/down_secret
 cp -H ~/scripts/gtk-bookmarks ~/.gtk-bookmarks
 sudo start sabnzbd
+
+# Build Go from tip every 5 days
+# (not a cron job because it needs an attached terminal)
+cd ~/go/src
+if [ $(( `date +%-d` % 5 )) -eq 0 ]; then
+    hg sync
+    ./all.bash
+else
+    echo "Building go not needed today, `date +%-d`"
+fi
