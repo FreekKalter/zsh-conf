45da8ba5c40bcdbc15ee9cd064d2a2c62584e771
Freek Kalter <freek@kalteronline.org>
2013-07-18 22:22:59 +0200

    proper bash recursive links

--- create_links.sh
+++ create_links.sh
@@ -6,4 +6,26 @@ ln -f .zsh_aliases ~/.zsh_aliases
 ln -f .tmux.conf ~/.tmux.conf
 ln -f freek.zsh-theme ~/.oh-my-zsh/themes/freek.zsh-theme
 
+# $1 source $2 destination
+recurseDir(){
+    cd $1
+    if [ ! -e $2 ]; then
+        mkdir $2
+    fi
+    for i in *
+    do
+        if [ -d $i ]
+        then
+            recurseDir "$1/$i" "$2/$i"
+        else
+            ln -sf "$1/$i" "$2/$i"
+        fi
+    done
+}
+
+destination=~/scripts
+if [ ! -e $destination ]; then
+    mkdir $destination
+fi
+recurseDir `pwd` $destination
 echo "links created"
