f10659d80b2368ef4b08801d5c0038278230b1b0
Freek Kalter <freek@kalteronline.org>
2013-07-20 20:45:22 +0200

    hopefully it will work as inteded everywhere
    The script can be used as a git hook in project_dir/.git/hooks/post-commit
    in this case the $GIT_DIR env variable will be set and you can use this to
    navigate to the root of the project_dir to start linking.
    If you have this file in the root of your prject_dir you can call it manualy
    from the commandline to in that case the location of this script itself will
    be used as the working directory.

--- create_links.sh
+++ create_links.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-if [ ! -z $GITDIR ]; then # to call it manualy
+if [ ! -z $GITDIR ]; then
+    # The script can be used as a git hook in project_dir/.git/hooks/post-commit
+    # in this case the $GIT_DIR env variable will be set and you can use this to
+    # navigate to the root of the project_dir to start linking.
     cd $GIT_DIR/..
+else
+    # If you have this file in the root of your prject_dir you can call it manualy
+    # from the commandline to in that case the location of this script itself will
+    # be used as the working directory.
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 fi
+
 ln -f .zshrc ~/.zshrc
 ln -f .zsh_aliases ~/.zsh_aliases
 ln -f .tmux.conf ~/.tmux.conf
