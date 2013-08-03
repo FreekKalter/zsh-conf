9261b62a3a7fcefc10dabc77ce753af3cd0a5b5d
Freek Kalter <freek@kalteronline.org>
2013-08-03 23:47:21 +0200

    create_links does not recursively link scripts anymore

--- create_links.sh
+++ create_links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -z $GITDIR ]; then
+if [ ! -z $GIT_DIR ]; then
     # The script can be used as a git hook in project_dir/.git/hooks/post-commit
     # in this case the $GIT_DIR env variable will be set and you can use this to
     # navigate to the root of the project_dir to start linking.
@@ -18,30 +18,31 @@ ln -f .zsh_aliases ~/.zsh_aliases
 ln -f .tmux.conf ~/.tmux.conf
 ln -f freek.zsh-theme ~/.oh-my-zsh/themes/freek.zsh-theme
 
-# $1 source $2 destination
-recurseDir(){
-    cd $1
-    if [ ! -e $2 ]; then
-        mkdir $2
-    fi
-    for i in *
-    do
-        if [ -d $i ]
-        then
-            recurseDir "$1/$i" "$2/$i"
-            # recurseDir will change the current working dir, so switch back
-            # to the working dir for this loop
-            cd $1
-        else
-            ln -sf "$1/$i" "$2/$i"
-        fi
-    done
-}
-
-destination=~/scripts
-if [ ! -e $destination ]; then
-    mkdir $destination
-fi
-recurseDir "`pwd`/scripts" $destination
-# remove dead links, files wich are deleted in git
-find $destination -type l ! -exec test -r {} \; -delete
+echo "links created"
+# # $1 source $2 destination
+# recurseDir(){
+#     cd $1
+#     if [ ! -e $2 ]; then
+#         mkdir $2
+#     fi
+#     for i in *
+#     do
+#         if [ -d $i ]
+#         then
+#             recurseDir "$1/$i" "$2/$i"
+#             # recurseDir will change the current working dir, so switch back
+#             # to the working dir for this loop
+#             cd $1
+#         else
+#             ln -sf "$1/$i" "$2/$i"
+#         fi
+#     done
+# }
+#
+# destination=~/scripts
+# if [ ! -e $destination ]; then
+#     mkdir $destination
+# fi
+# recurseDir "`pwd`/scripts" $destination
+# # remove dead links, files wich are deleted in git
+# find $destination -type l ! -exec test -r {} \; -delete
