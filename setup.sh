becc183dfdc6f3684f26236d0aea548052bbf81d
Freek Kalter <fkalter@minimal.home>
2014-04-26 14:21:24 +0200

    use absolute link paths

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-ln -fs .zshrc ~/.zshrc
-ln -fs .zsh_aliases ~/.zsh_aliases
-ln -fs .tmux.conf ~/.tmux.conf
-ln -fs freek.zsh-theme ~/.oh-my-zsh/themes/freek.zsh-theme
+ln -fs `pwd`/.zshrc ~/.zshrc
+ln -fs `pwd`/.zsh_aliases ~/.zsh_aliases
+ln -fs `pwd`/.tmux.conf ~/.tmux.conf
+ln -fs `pwd`/freek.zsh-theme ~/.oh-my-zsh/themes/freek.zsh-theme
