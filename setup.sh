8150eecb6254eb849b887ecdf17ef0478eb0ee62
Freek Kalter <freek@kalteronline.org>
2014-04-26 14:01:05 +0200

    created setup script;

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+ln -fs .zshrc ~/.zshrc
+ln -fs .zsh_aliases ~/.zsh_aliases
+ln -fs .tmux.conf ~/.tmux.conf
+ln -fs freek.zsh-theme ~/.oh-my-zsh/themes/freek.zsh-theme
