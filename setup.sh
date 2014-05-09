70d9f4dbc40a683d8d5b61ada09c1a3cabcf6d8a
Freek Kalter <freek@kalteronline.org>
2014-05-09 19:04:23 +0200

    install fasd with setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ ln -fs `pwd`/.zshrc ~/.zshrc
 ln -fs `pwd`/.zsh_aliases ~/.zsh_aliases
 ln -fs `pwd`/.tmux.conf ~/.tmux.conf
 ln -fs `pwd`/freek.zsh-theme ~/.oh-my-zsh/themes/freek.zsh-theme
+
+git clone https://github.com/clvv/fasd.git ~/fasd
+cd ~/fasd
+sudo make install
